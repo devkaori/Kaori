699d87263c9f193c91cab4246126faafc57ab121
Lium <71281668+Liivm@users.noreply.github.com>
2023-05-18 20:13:33 +0200

    Traduction de la commande

--- src/commands/economy/beg.js
+++ src/commands/economy/beg.js
@@ -19,10 +19,10 @@ module.exports = async (client, interaction, args) => {
         else {
 
             client.succNormal({
-                text: `You've begged for some money!`,
+                text: "Vous avez demandé de l'argent !",
                 fields: [
                     {
-                        name: `${client.emotes.economy.coins}┆Amount`,
+                        name: "Montant",
                         value: `$${amount}`,
                         inline: true
                     }
@@ -46,5 +46,3 @@ module.exports = async (client, interaction, args) => {
         }
     })
 }
-
- 
