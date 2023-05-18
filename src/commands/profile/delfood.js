4ef0119d8638a5f66750d730101e0f3814e78c90
Lium <71281668+Liivm@users.noreply.github.com>
2023-05-18 21:50:39 +0200

    Traduction de la commande

--- src/commands/profile/delfood.js
+++ src/commands/profile/delfood.js
@@ -10,7 +10,7 @@ module.exports = async (client, interaction, args) => {
 
             if (data && data.Food) {
                 if (!data.Food.includes(food)) {
-                    return client.errNormal({ error: `That food doesn't exist in the database!`, type: 'editreply' }, interaction);
+                    return client.errNormal({ error: `Ce plat n'existe pas dans la base de données !`, type: 'editreply' }, interaction);
                 }
 
                 const filtered = data.Food.filter((target) => target !== food);
@@ -20,9 +20,9 @@ module.exports = async (client, interaction, args) => {
                 });
             }
             client.succNormal({
-                text: "Removed your food",
+                text: "Votre plat a été supprimé",
                 fields: [{
-                    name: "🥐┆Food",
+                    name: "Plat",
                     value: `\`\`\`${food}\`\`\``,
                     inline: true,
                 }],
@@ -30,10 +30,8 @@ module.exports = async (client, interaction, args) => {
             }, interaction);
         }
         else {
-            return client.errNormal({ error: "No profile found! Open a profile with createprofile", type:'editreply' }, interaction);
+            return client.errNormal({ error: "Aucun profil trouvé ! Ouvrez un profil avec la commande 'createprofile'", type:'editreply' }, interaction);
         }
     })
 
 }
-
- 
