586c7c7ca277ed768f981f530dc6dc6913d8d137
Lium <71281668+Liivm@users.noreply.github.com>
2023-05-18 23:08:20 +0200

    Changement des liens

--- src/config/bot.js
+++ src/config/bot.js
@@ -8,7 +8,7 @@ module.exports = {
     discord: {
         id: process.env.DISCORD_ID,
         prefix: '!',
-        botInvite: `https://discord.com/oauth2/authorize?&client_id=840212110817755157&scope=applications.commands+bot&permissions=8`,
+        botInvite: `https://discord.com/oauth2/authorize?&client_id=855107430693077033&scope=applications.commands+bot&permissions=8`,
         serverInvite: "https://discord.gg/HyybNmz2Ww",
     },
 
