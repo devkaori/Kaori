b455c3387d242bd475428c73945a664475444516
Lium <71281668+Liivm@users.noreply.github.com>
2023-05-18 21:49:41 +0200

    Traduction de la commande

--- src/commands/profile/delete.js
+++ src/commands/profile/delete.js
@@ -7,18 +7,16 @@ module.exports = async (client, interaction, args) => {
         if (data) {
             Schema.findOneAndDelete({ Guild: interaction.guild.id, User: interaction.user.id }).then(() => {
                 client.succNormal({
-                    text: "Your profile was deleted!",
+                    text: "Votre profil a été supprimé !",
                     type: 'editreply'
                 }, interaction);
             })
         }
         else {
             client.errNormal({
-                error: 'No profile found!',
+                error: 'Aucun profil trouvé !',
                 type: 'editreply'
             }, interaction)
         }
     })
 }
-
- 
