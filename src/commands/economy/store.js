cd77a43b428e84ea99f34c9a8f4cd62d8769c642
Lium <71281668+Liivm@users.noreply.github.com>
2023-05-18 20:36:32 +0200

    Traduction de la commande

--- src/commands/economy/store.js
+++ src/commands/economy/store.js
@@ -5,22 +5,20 @@ const store = require("../../database/models/economyStore");
 module.exports = async (client, interaction, args, message) => {
     store.find({ Guild: interaction.guild.id }, async (err, storeData) => {
         if (storeData && storeData.length > 0) {
-            const lb = storeData.map(e => `**<@&${e.Role}>** - ${client.emotes.economy.coins} $${e.Amount} \n**To buy:** \`buy ${e.Role}\``);
+            const lb = storeData.map(e => `**<@&${e.Role}>** - ${client.emotes.economy.coins} $${e.Amount} \n**Pour acheter :** \`buy ${e.Role}\``);
 
-            await client.createLeaderboard(`🛒・${interaction.guild.name}'s Store`, lb, interaction);
+            await client.createLeaderboard(`Boutique de ${interaction.guild.name}`, lb, interaction);
             client.embed({ 
-                title: `🛒・Bot's Store`, 
-                desc: `**Fishingrod** - ${client.emotes.economy.coins} $100 \n**To buy:** \`buy fishingrod\``, 
+                title: `Boutique du bot`, 
+                desc: `**Canne à pêche** - ${client.emotes.economy.coins} $100 \n**Pour acheter :** \`buy fishingrod\``, 
             }, interaction.channel);
         }
         else {
             client.errNormal({ 
-                error: `No store found in this guild!`, 
+                error: `Aucune boutique trouvée dans ce serveur !`, 
                 type: 'editreply' 
             }, interaction);
         }
     })
 
 }
-
- 
