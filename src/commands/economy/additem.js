10271cee2b729f673ce555c4df8b5bb95dbc4c71
Lium <71281668+Liivm@users.noreply.github.com>
2023-05-18 20:09:46 +0200

    Traduction de la commande

--- src/commands/economy/additem.js
+++ src/commands/economy/additem.js
@@ -13,15 +13,15 @@ module.exports = async (client, interaction, args) => {
     const role = interaction.options.getRole('role');
     let amount = interaction.options.getNumber('amount');
 
-    if (!role || !amount) return client.errUsage({ usage: "additem [role] [amount]", type: 'editreply' }, interaction);
+    if (!role || !amount) return client.errUsage({ usage: "additem [role] [montant]", type: 'editreply' }, interaction);
 
-    if (isNaN(amount)) return client.errNormal({ error: "Enter a valid number!", type: 'editreply' }, interaction);
+    if (isNaN(amount)) return client.errNormal({ error: "Entrez un nombre valide !", type: 'editreply' }, interaction);
 
-    if(role == interaction.guild.roles.everyone) return client.errNormal({ error: "You cannot add the everyone role to the store!", type: 'editreply' }, interaction);
+    if(role == interaction.guild.roles.everyone) return client.errNormal({ error: "Vous ne pouvez pas ajouter le rôle @everyone au magasin !", type: 'editreply' }, interaction);
 
     store.findOne({ Guild: interaction.guild.id, Role: role.id }, async (err, storeData) => {
         if (storeData) {
-            client.errNormal({ error: `This role is already in the store!`, type: 'editreply' }, interaction);
+            client.errNormal({ error: "Ce rôle est déjà dans le magasin !", type: 'editreply' }, interaction);
         }
         else {
 
@@ -32,15 +32,15 @@ module.exports = async (client, interaction, args) => {
             }).save();
 
             client.succNormal({
-                text: `The role was added to the store!`,
+                text: "Le rôle a été ajouté au magasin !",
                 fields: [
                     {
-                        name: `🛒┆Role`,
+                        name: "Rôle",
                         value: `<@&${role.id}>`,
                         inline: true
                     },
                     {
-                        name: `${client.emotes.economy.coins}┆Amount`,
+                        name: `Montant`,
                         value: `$${amount}`,
                         inline: true
                     }
@@ -50,5 +50,3 @@ module.exports = async (client, interaction, args) => {
         }
     })
 }
-
- 
