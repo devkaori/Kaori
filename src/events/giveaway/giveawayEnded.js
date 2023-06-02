8a3949538bcbfc6699bfcae93bbdb93624942d8f
Masha Whisperer <97138114+MashaWhisperer@users.noreply.github.com>
2023-06-02 15:33:35 +0200

    Update giveawayEnded.js

--- src/events/giveaway/giveawayEnded.js
+++ src/events/giveaway/giveawayEnded.js
@@ -4,16 +4,16 @@ module.exports = (client, giveaway, winners) => {
     winners.forEach((member) => {
         client.embed({
             title: `Concours terminé`,
-            desc: `Félicitations ${member.user.username} ! Vous avez remporté le concours !`,
+            desc: `- <:icons_announce:1114165367607152660> Félicitations ${member.user.username} ! Vous avez remporté le concours !`,
             fields: [
                 {
-                    name: `Prix`,
-                    value: `${giveaway.prize}`,
+                    name: `<:icons_gift:1114165331213172796> Prix`,
+                    value: `<:Icon_Arrow_Right:1114167654777888798> ${giveaway.prize}`,
                     inline: true
                 },
                 {
-                    name: `Concours`,
-                    value: `[Cliquez ici](https://discordapp.com/channels/${giveaway.message.guildId}/${giveaway.message.channelId}/${giveaway.message.id})`,
+                    name: `<:icons_tada:1114165284127916072> Concours`,
+                    value: `<:Icon_Arrow_Right:1114167654777888798> [Cliquez ici](https://discordapp.com/channels/${giveaway.message.guildId}/${giveaway.message.channelId}/${giveaway.message.id})`,
                     inline: true
                 }
             ]
