cf077cab37e8e5d46a2362e7228ab257fcf155ec
Masha Whisperer <97138114+MashaWhisperer@users.noreply.github.com>
2023-06-02 15:08:49 +0200

    Ajout d'emotes

--- src/events/giveaway/giveawayReactionAdded.js
+++ src/events/giveaway/giveawayReactionAdded.js
@@ -2,6 +2,6 @@ const Discord = require('discord.js');
 
 module.exports = (client, giveaway, member, reaction) => {
     client.succNormal({
-        text: `Votre participation à [ce concours](https://discordapp.com/channels/${giveaway.message.guildId}/${giveaway.message.channelId}/${giveaway.message.id}) a été approuvée.`
+        text: `<:icons_gift:1114165331213172796> Votre participation à [ce concours](https://discordapp.com/channels/${giveaway.message.guildId}/${giveaway.message.channelId}/${giveaway.message.id}) a été approuvée.`
     }, member).catch(() => { });
 };
