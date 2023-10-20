7758aa8dced3d021f20477d27ad66b5c910ca596
Masha <97138114+cosymasha@users.noreply.github.com>
2023-10-20 16:30:11 +0200

    Update hug.js

--- src/interactions/Command/hug.js
+++ src/interactions/Command/hug.js
@@ -1,38 +1,61 @@
-const { CommandInteraction, Client, MessageActionRow, MessageButton } = require('discord.js');
-const axios = require('axios');
+const { SlashCommandBuilder } = require('discord.js');
+const fetch = require('node-fetch');  // Assurez-vous d'installer le package
 
 module.exports = {
-    data: {
-        name: 'calin',
-        description: 'Fais un câlin à une personne avec un gif',
-        options: [{
-            name: 'user',
-            type: 'USER',
-            description: 'La personne à qui tu veux faire un câlin',
-            required: true
-        }]
-    },
+    data: new SlashCommandBuilder()
+        .setName('hug')
+        .setDescription('Faire un câlin à quelqu\'un')
+        .addUserOption(option => 
+            option.setName('target')
+                  .setDescription('La personne à qui vous voulez faire un câlin')
+                  .setRequired(true)),
 
-    /** 
-     * @param {Client} client
-     * @param {CommandInteraction} interaction
-     */
-
-    run: async (client, interaction) => {
+    run: async (client, interaction, args) => {
         await interaction.deferReply({ fetchReply: true });
 
-        const targetUser = interaction.options.getUser('user');
-        
-        try {
-            const response = await axios.get('http://api.nekos.fun:8080/api/hug');
-            const embed = new Discord.MessageEmbed()
-                .setTitle(`${interaction.user.username} fait un câlin à ${targetUser.username}!`)
-                .setImage(response.data.url)
-                .setColor('#FFC0CB');
+        const target = interaction.options.getUser('target');
 
-            interaction.editReply({ embeds: [embed] });
-        } catch (error) {
-            interaction.editReply('Oups, il y a eu une erreur. Essaye plus tard!');
-        }
-    }
+        // Récupérer une image de câlin depuis nekos.fun
+        const response = await fetch('https://api.nekos.fun:8080/api/hug');
+        const data = await response.json();
+        const hugImageUrl = data.image; // ou le chemin approprié dans la réponse
+
+        await interaction.editReply({
+            content: `<@${interaction.user.id}> fait un câlin à <@${target.id}> ! 🤗`,
+            embeds: [{
+                title: 'Un câlin chaleureux !',
+                image: {
+                    url: hugImageUrl
+                }
+            }],
+            components: [
+                {
+                    type: 'ACTION_ROW',
+                    components: [
+                        {
+                            type: 'BUTTON',
+                            customId: 'hug_back',
+                            label: 'Rendre le câlin',
+                            style: 'PRIMARY'
+                        }
+                    ]
+                }
+            ]
+        });
+
+        const filter = i => {
+            i.deferUpdate();
+            return i.customId === 'hug_back' && i.user.id === target.id;
+        };
+
+        const collector = interaction.channel.createMessageComponentCollector({ filter, time: 60000 });
+
+        collector.on('collect', async i => {
+            if (i.customId === 'hug_back') {
+                await interaction.followUp({
+                    content: `<@${target.id}> a rendu le câlin à <@${interaction.user.id}> ! 🤗`,
+                });
+            }
+        });
+    },
 };
