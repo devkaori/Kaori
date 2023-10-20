497519a7cb47e0b2e4fdf9eeae6d41d61f0cc674
Masha <97138114+cosymasha@users.noreply.github.com>
2023-10-20 16:41:41 +0200

    Update help.js

--- src/interactions/Command/help.js
+++ src/interactions/Command/help.js
@@ -1,6 +1,5 @@
-const { CommandInteraction, Client } = require('discord.js');
+const { CommandInteraction, Client, MessageActionRow, MessageSelectMenu } = require('discord.js');
 const { SlashCommandBuilder } = require('discord.js');
-const Discord = require('discord.js');
 const moment = require("moment");
 require("moment-duration-format");
 
@@ -17,9 +16,10 @@ module.exports = {
 
     run: async (client, interaction, args) => {
         await interaction.deferReply({ fetchReply: true });
-        const row = new Discord.ActionRowBuilder()
+
+        const row = new MessageActionRow()
             .addComponents(
-                new Discord.StringSelectMenuBuilder()
+                new MessageSelectMenu()
                     .setCustomId('Bot-helppanel')
                     .setPlaceholder('Rien de sélectionné')
                     .addOptions([
@@ -44,21 +44,30 @@ module.exports = {
                     ]),
             );
 
-        return client.embed({
+        const imageEmbed = {
+            image: {
+                url: "https://i.imgur.com/FkGNk5Y.jpg",  // Remplacez par l'URL de votre image
+            },
+        };
+
+        const helpEmbed = {
             title: `Panneau d'aide`,
-            desc: `Bienvenue dans le panneau d'aide du bot ! Nous avons préparé une petite présentation pour vous aider ! Faites un choix via le menu ci-dessous`,
+            description: `Bienvenue dans le panneau d'aide du bot ! Nous avons préparé une petite présentation pour vous aider ! Faites un choix via le menu ci-dessous`,
             fields: [
                 {
-                    name: `<:icons_info:1114167700999118878> Le menu ne fonctionne pas ?`,
+                    name: `Le menu ne fonctionne pas ?`,
                     value: `Essayez d'envoyer à nouveau la commande. Si vous n'obtenez aucune réaction, assurez-vous de signaler le bug !`
                 },
                 {
-                    name: `<:icons_Bugs:1114168638992306237> Vous avez trouvé un bug ?`,
+                    name: `Vous avez trouvé un bug ?`,
                     value: `Signalez-le avec \`/report bug\``
                 },
             ],
-            components: [row],
-            type: 'editreply'
-        }, interaction)
+        };
+
+        await interaction.editReply({
+            embeds: [imageEmbed, helpEmbed],
+            components: [row]
+        });
     },
 };
