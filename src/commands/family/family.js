bac462024dd4bccc9e339395206cf07d03d44eb5
Masha <97138114+cosymasha@users.noreply.github.com>
2023-10-20 16:21:06 +0200

    Update family.js

--- src/commands/family/family.js
+++ src/commands/family/family.js
@@ -1,30 +1,30 @@
+const Discord = require('discord.js');
+
 const Schema = require("../../database/models/family");
 
 module.exports = async (client, interaction, args) => {
+
     const target = interaction.options.getUser('user') || interaction.user;
+
     const data = await Schema.findOne({ Guild: interaction.guild.id, User: target.id });
 
-    await interaction.editReply({ 
-        embeds: [
+    client.embed({
+        title: `Famille de ${target.username}`,
+        thumbnail: target.avatarURL({ size: 1024 }),
+        fields: [
             {
-                title: `Famille de ${target.username}`,
-                thumbnail: { url: target.avatarURL({ size: 1024 }) },
-                image: "https://i.imgur.com/FkGNk5Y.jpg",
-                fields: [
-                    {
-                        name: "Partenaire",
-                        value: data && data.Partner ? `<@!${data.Partner}>` : "Cet utilisateur n'est pas marié(e)"
-                    },
-                    {
-                        name: "Parent",
-                        value: data && data.Parent.length > 0 ? `**${data.Parent.join(", ")}**` : "Cet utilisateur n'a pas de parents"
-                    },
-                    {
-                        name: "Enfants",
-                        value: data && data.Children.length > 0 ? `**${data.Children.join(", ")}**` : "Cet utilisateur n'a pas d'enfants"
-                    }
-                ]
+                name: `Partenaire`,
+                value: `${data && data.Partner ? `<@!${data.Partner}>` : `Cet utilisateur n'est pas marié(e)`}`
+            },
+            {
+                name: `Parent`,
+                value: `${data && data.Parent.length > 0 ? `**${data.Parent.join(", ")}**` : `Cet utilisateur n'a pas de parents`}`
+            },
+            {
+                name: `Enfants`,
+                value: `${data && data.Children.length > 0 ? `**${data.Children.join(", ")}**` : `Cet utilisateur n'a pas d'enfants`}`
             }
-        ]
-    });
+        ],
+        type: 'editreply'
+    }, interaction)
 }
