a4b13cd2a88325af91ca4d5571bca2990a7a9611
Masha <97138114+cosymasha@users.noreply.github.com>
2023-10-27 19:31:21 +0200

    Update giveawayReactionAdded.js

--- src/events/giveaway/giveawayReactionAdded.js
+++ src/events/giveaway/giveawayReactionAdded.js
@@ -2,6 +2,6 @@ const Discord = require('discord.js');
 
 module.exports = (client, giveaway, member, reaction) => {
     client.succNormal({
-        text: `<:icons_gift:1114165331213172796> Votre participation à [ce concours](https://discordapp.com/channels/${giveaway.message.guildId}/${giveaway.message.channelId}/${giveaway.message.id}) a été approuvée.`
+        text: `<:white_heart:1130912421213646908> Votre participation à [ce concours](https://discordapp.com/channels/${giveaway.message.guildId}/${giveaway.message.channelId}/${giveaway.message.id}) a été approuvée.`
     }, member).catch(() => { });
 };
