b4bc86a615a2f3a311f395429636af89572f92b1
Masha <97138114+cosymasha@users.noreply.github.com>
2023-10-27 20:10:05 +0200

    Modification des emotes

--- src/interactions/Command/help.js
+++ src/interactions/Command/help.js
@@ -1,5 +1,6 @@
-const { CommandInteraction, Client, ActionRowBuilder, StringSelectMenuBuilder } = require('discord.js');
+const { CommandInteraction, Client } = require('discord.js');
 const { SlashCommandBuilder } = require('discord.js');
+const Discord = require('discord.js');
 const moment = require("moment");
 require("moment-duration-format");
 
@@ -16,58 +17,48 @@ module.exports = {
 
     run: async (client, interaction, args) => {
         await interaction.deferReply({ fetchReply: true });
-
-        const row = new MessageActionRow()
+        const row = new Discord.ActionRowBuilder()
             .addComponents(
-                new StringSelectMenuBuilder()
+                new Discord.StringSelectMenuBuilder()
                     .setCustomId('Bot-helppanel')
                     .setPlaceholder('Rien de sélectionné')
                     .addOptions([
                         {
                             label: `Commandes`,
                             description: `Afficher les commandes du bot !`,
-                            emoji: "1114167872646819961",
+                            emoji: "1163106294073401354",
                             value: "commands-Bothelp",
                         },
                         {
                             label: `Invitation`,
                             description: `Inviter le bot sur votre serveur`,
-                            emoji: "1114167747627208815",
+                            emoji: "1163106292227915796",
                             value: "invite-Bothelp",
                         },
                         {
                             label: `Serveur de support`,
                             description: `Rejoindre le serveur de support`,
-                            emoji: "1114167700999118878",
+                            emoji: "1163106279187820544",
                             value: "support-Bothelp",
                         },
                     ]),
             );
 
-        const imageEmbed = {
-            image: {
-                url: "https://i.imgur.com/FkGNk5Y.jpg",  // Remplacez par l'URL de votre image
-            },
-        };
-
-        const helpEmbed = {
+        return client.embed({
             title: `Panneau d'aide`,
-            description: `Bienvenue dans le panneau d'aide du bot ! Nous avons préparé une petite présentation pour vous aider ! Faites un choix via le menu ci-dessous`,
+            desc: `Bienvenue dans le panneau d'aide du bot ! Nous avons préparé une petite présentation pour vous aider ! Faites un choix via le menu ci-dessous`,
             fields: [
                 {
-                    name: `Le menu ne fonctionne pas ?`,
+                    name: `<:white_heart:1130912421213646908> Le menu ne fonctionne pas ?`,
                     value: `Essayez d'envoyer à nouveau la commande. Si vous n'obtenez aucune réaction, assurez-vous de signaler le bug !`
                 },
                 {
-                    name: `Vous avez trouvé un bug ?`,
-                    value: `Signalez-le avec \`/report bug\``
+                    name: `<<a:8a_exclamation:1163192046392189059> Vous avez trouvé un bug ?`,
+                    value: `Signalez-le [nous](https://discord.gg/7S28GvPsZQ)``
                 },
             ],
-        };
-
-        await interaction.editReply({
-            embeds: [imageEmbed, helpEmbed],
-            components: [row]
-        });
+            components: [row],
+            type: 'editreply'
+        }, interaction)
     },
 };
