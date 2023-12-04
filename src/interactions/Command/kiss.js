1a4f164bb1f5d11fe01515f02a4f4a7a80af2254
Masha <97138114+cosymasha@users.noreply.github.com>
2023-12-04 15:36:00 +0100

    Create kiss.js

---
+++ src/interactions/Command/kiss.js
@@ -0,0 +1,37 @@
+const { CommandInteraction, Client } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
+const fetch = require('node-fetch');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('kiss')
+        .setDescription(`Faire un bisous à un autre membre`)
+        .addUserOption(option =>
+            option.setName('membre')
+                .setDescription('Le membre à qui faire un bisous')
+                .setRequired(true)),
+
+    run: async (client, interaction, args) => {
+        await interaction.deferReply({ fetchReply: true });
+
+        const response = await fetch('http://api.nekos.fun:8080/api/kiss');
+        const data = await response.json();
+
+        const member = interaction.options.getMember('membre');
+
+        if (member) {
+            const embed = {
+                color: '#faffca'
+                title: `Bisous ❤️`,
+                description: `<a:yuiazu_cuddle:1163075413598285954> ${interaction.user} fait un bisous à ${member}`,
+                image: {
+                    url: data.image,
+                },
+            };
+
+            return interaction.editReply({ embeds: [embed] });
+        } else {
+            interaction.reply({ content: '<:non:1161484000272060548> Veuillez mentionner un membre à qui faire un bisous.', ephemeral: true });
+        }
+    },
+};
