5d858ddc6dfa0afe5c037bd4fa7aacad2ddd8cc2
Masha <97138114+cosymasha@users.noreply.github.com>
2023-12-05 15:01:20 +0100

    Modification des emotes

--- src/handlers/helppanel/support.js
+++ src/handlers/helppanel/support.js
@@ -17,19 +17,19 @@ module.exports = async (client) => {
                                 {
                                     label: `Commandes`,
                                     description: `Afficher les commandes de ${client.user.username} !`,
-                                    emoji: "1114167872646819961",
+                                    emoji: "1163106294073401354",
                                     value: "commands-Bothelp",
                                 },
                                 {
                                     label: `Invitation`,
                                     description: `Inviter ${client.user.username} sur votre serveur`,
-                                    emoji: "1114167747627208815",
+                                    emoji: "1163106292227915796",
                                     value: "invite-Bothelp",
                                 },
                                 {
-                                    label: `Serveur de support`,
+                                    label: `Communauté`,
                                     description: `Rejoindre le serveur de support`,
-                                    emoji: "1114167700999118878",
+                                    emoji: "1163106279187820544",
                                     value: "support-Bothelp",
                                 },
                             ]),
