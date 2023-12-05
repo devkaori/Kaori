2a26de6a2fd0fedeab283b69d352b77ca6ec3999
Masha <97138114+cosymasha@users.noreply.github.com>
2023-12-05 15:55:12 +0100

    Update commands.js

--- src/handlers/helppanel/commands.js
+++ src/handlers/helppanel/commands.js
@@ -229,7 +229,7 @@ module.exports = async (client) => {
 
                 client.embed({
                     title: `Panneau d'aide`,
-                    image: 'https://i.imgur.com/qroUk6M.jpg',
+                    image: 'https://i.imgur.com/AWDFpqV.jpg',
                     fields: fields.slice(0, 24),
                     components: [row2, row],
                     type: 'edit'
