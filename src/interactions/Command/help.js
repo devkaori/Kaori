ea32580e77f6b4aacf9fc417e4c32a9f0c6bc26e
Masha <97138114+cosymasha@users.noreply.github.com>
2023-12-08 01:39:07 +0100

    Changement de l'image

--- src/interactions/Command/help.js
+++ src/interactions/Command/help.js
@@ -46,7 +46,7 @@ module.exports = {
 
         return client.embed({
             title: `Panneau d'aide`,
-            image: 'https://i.imgur.com/AWDFpqV.jpg',
+            image: 'https://i.imgur.com/umYjAX0.png',
             desc: `Bienvenue dans le panneau d'aide du bot ! On a préparé une petite présentation pour te guider ! Choisis ce qui te branche dans le menu ci-dessous, et on part en aventure !`,
             fields: [
                 {
