4521f3eb90faa341607597424a71715974bc471a
Masha <97138114+cosymasha@users.noreply.github.com>
2023-12-08 01:55:33 +0100

    Mis à jour

--- src/commands/invites/leaderboard.js
+++ src/commands/invites/leaderboard.js
@@ -6,11 +6,11 @@ module.exports = async (client, interaction, args) => {
     const rawLeaderboard = await Schema.find({ Guild: interaction.guild.id }).sort(([['Invites', 'descending']]));
 
     if (!rawLeaderboard) return client.errNormal({
-        error: `<:non:1161484000272060548> Aucune donnée trouvée !`,
+        error: `Aucune donnée trouvée !`,
         type: 'editreply'
     }, interaction);
 
-    const lb = rawLeaderboard.map(e => `<:dash:1146416325892247684> \`${e.Invites}\` invitations pour <@!${e.User}>`);
+    const lb = rawLeaderboard.map(e => `- \`${e.Invites}\` invitations pour <@!${e.User}>`);
 
     await client.createLeaderboard(`Tableau de score`, lb, interaction);
 }
