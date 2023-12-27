bc255ea9f1f7ed8ee0491786384c59ee781a7e78
Masha <97138114+cosymasha@users.noreply.github.com>
2023-12-27 18:52:02 +0100

    Bouton

--- src/commands/setup/ticketpanel.js
+++ src/commands/setup/ticketpanel.js
@@ -5,8 +5,8 @@ module.exports = async (client, interaction, args) => {
     const name = interaction.options.getString('name');
     const description = interaction.options.getString('description');
     const option1 = interaction.options.getString('option1');
-    const option2 = interaction.options.getString('option2') || 'Option 2';
-    const option3 = interaction.options.getString('option3') || 'Option 3';
+    const option2 = interaction.options.getString('option2');
+    const option3 = interaction.options.getString('option3');
 
     // Recherche des données du ticket dans la base de données
     ticketSchema.findOne({ Guild: interaction.guild.id }, async (err, ticketData) => {
@@ -15,27 +15,36 @@ module.exports = async (client, interaction, args) => {
             const channel = interaction.guild.channels.cache.get(ticketData.Channel);
 
             // Création des boutons
+            const components = [];
+
             const button1 = new Discord.ButtonBuilder()
                 .setCustomId('Bot_openticket1')
                 .setLabel(option1 || 'Option 1')
                 .setStyle(Discord.ButtonStyle.Primary)
                 .setEmoji('<:Tickets:1189624711974498325>');
+            components.push(button1);
 
-            const button2 = new Discord.ButtonBuilder()
-                .setCustomId('Bot_openticket2')
-                .setLabel(option2)
-                .setStyle(Discord.ButtonStyle.Primary)
-                .setEmoji('<:Tickets:1189624711974498325>');
+            if (option2) {
+                const button2 = new Discord.ButtonBuilder()
+                    .setCustomId('Bot_openticket2')
+                    .setLabel(option2)
+                    .setStyle(Discord.ButtonStyle.Primary)
+                    .setEmoji('<:Tickets:1189624711974498325>');
+                components.push(button2);
+            }
 
-            const button3 = new Discord.ButtonBuilder()
-                .setCustomId('Bot_openticket3')
-                .setLabel(option3)
-                .setStyle(Discord.ButtonStyle.Primary)
-                .setEmoji('<:Tickets:1189624711974498325>');
+            if (option3) {
+                const button3 = new Discord.ButtonBuilder()
+                    .setCustomId('Bot_openticket3')
+                    .setLabel(option3)
+                    .setStyle(Discord.ButtonStyle.Primary)
+                    .setEmoji('<:Tickets:1189624711974498325>');
+                components.push(button3);
+            }
 
             // Création de la rangée d'actions
             const row = new Discord.ActionRowBuilder()
-                .addComponents(button1, button2, button3);
+                .addComponents(...components);
 
             // Envoi du message embed dans le canal
             client.embed({
