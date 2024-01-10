df9d7a0fa7d9cd4e28ad17ec35bdd3d8732c4c43
Masha <97138114+cosymasha@users.noreply.github.com>
2024-01-10 17:23:20 +0100

    Update hug.js

--- src/interactions/Command/hug.js
+++ src/interactions/Command/hug.js
@@ -8,7 +8,7 @@ module.exports = {
         .setDescription(`Faire un câlin à un autre membre`)
         .addUserOption(option =>
             option.setName('membre')
-                .setDescription('Le membre à câliner')
+                .setDescription('Le membre à qui faire un câlin')
                 .setRequired(true)),
 
     run: async (client, interaction, args) => {
@@ -17,13 +17,15 @@ module.exports = {
         const response = await fetch('http://api.nekos.fun:8080/api/hug');
         const data = await response.json();
 
+        const affectionPercentage = Math.floor(Math.random() * 100) + 1;
+
         const member = interaction.options.getMember('membre');
 
         if (member) {
             const embed = {
                 color: 0x2C2D31,
-                title: `Câlin ❤️`,
-                description: `<a:yuiazu_cuddle:1163075413598285954> ${interaction.user} fait un câlin à ${member}`,
+                title: `Calin`,
+                description: `<:s_heart_devill_red:1194671427132063866> ${interaction.user} fait un câlin à ${member}\n- Niveau d'affection de ${affectionPercentage}%`,
                 image: {
                     url: data.image,
                 },
@@ -31,7 +33,7 @@ module.exports = {
 
             return interaction.editReply({ embeds: [embed] });
         } else {
-            interaction.reply({ content: '<:non:1161484000272060548> Veuillez mentionner un membre à câliner.', ephemeral: true });
+            interaction.reply({ content: '<:non:1161484000272060548> Veuillez mentionner un membre à qui faire un câlin.', ephemeral: true });
         }
     },
 };
