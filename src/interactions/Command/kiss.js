2628d830c2a9d2ddbe169c9b2557ea54bb9aef02
Masha <97138114+cosymasha@users.noreply.github.com>
2024-01-10 17:18:59 +0100

    Ajoût de pourcentage

--- src/interactions/Command/kiss.js
+++ src/interactions/Command/kiss.js
@@ -17,13 +17,15 @@ module.exports = {
         const response = await fetch('http://api.nekos.fun:8080/api/kiss');
         const data = await response.json();
 
+        const affectionPercentage = Math.floor(Math.random() * 100) + 1;
+
         const member = interaction.options.getMember('membre');
 
         if (member) {
             const embed = {
                 color: 0x2C2D31,
                 title: `Bisous ❤️`,
-                description: `<a:yuiazu_cuddle:1163075413598285954> ${interaction.user} fait un bisous à ${member}`,
+                description: `<:s_heart_devill_red:1194671427132063866> ${interaction.user} fait un bisous à ${member}\n- Niveau d'affection de ${affectionPercentage}%`,
                 image: {
                     url: data.image,
                 },
