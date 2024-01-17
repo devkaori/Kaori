4d3dbcc8b719f7ec8bc4e00a33b7f7e13e9268fa
Masha <97138114+cosymasha@users.noreply.github.com>
2024-01-17 04:45:42 +0100

    Update family.js

--- src/commands/family/family.js
+++ src/commands/family/family.js
@@ -6,7 +6,8 @@ module.exports = async (client, interaction, args) => {
     const data = await Schema.findOne({ Guild: interaction.guild.id, User: target.id });
 
     client.embed({
-        title: `Famille de \`${target.username}\``,
+        title: `{target.username} - Famille`,
+        image: `https://i.imgur.com/U9Fih4D.png`
         thumbnail: target.avatarURL({ size: 1024 }),
         fields: [
             {
@@ -14,8 +15,8 @@ module.exports = async (client, interaction, args) => {
                 value: data && data.Partner ? `<@!${data.Partner}>` : 'Cet utilisateur n\'est pas marié(e)'
             },
             {
-                name: 'Parent',
-                value: data && data.Parent.length > 0 ? `**${data.Parent.join(' | ')}**` : 'Cet utilisateur n\'a pas de parents'
+                name: 'Parents',
+                value: data && data.Parent.length > 0 ? data.Parent.map(parent => `- \`${parent}\``).join('\n') : 'Cet utilisateur n\'a pas de parents'
             },
             {
                 name: 'Enfants',
