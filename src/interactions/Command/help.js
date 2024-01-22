e9f4c9f1279b7a9032621c6595cf3b10a9b7d399
Masha <97138114+cosymasha@users.noreply.github.com>
2024-01-22 12:17:59 +0100

    Update help.js

--- src/interactions/Command/help.js
+++ src/interactions/Command/help.js
@@ -1,37 +1,66 @@
 const { CommandInteraction, Client } = require('discord.js');
 const { SlashCommandBuilder } = require('discord.js');
 const Discord = require('discord.js');
+const moment = require("moment");
+require("moment-duration-format");
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('help')
         .setDescription(`Obtenir de l'aide avec le bot`),
 
-    run: async (client, interaction) => {
-        await interaction.deferReply({ ephemeral: true });
+    /** 
+     * @param {Client} client
+     * @param {CommandInteraction} interaction
+     * @param {String[]} args
+     */
 
-        const menu = new Discord.ActionRowBuilder()
+    run: async (client, interaction, args) => {
+        await interaction.deferReply({ fetchReply: true });
+        const row = new Discord.ActionRowBuilder()
             .addComponents(
                 new Discord.StringSelectMenuBuilder()
                     .setCustomId('Bot-helppanel')
                     .setPlaceholder('Rien de sélectionné')
                     .addOptions([
-                        { label: `Commandes`, description: `Afficher les commandes du bot`, emoji: "1163106294073401354", value: "commands-Bothelp" },
-                        { label: `Invitation`, description: `Inviter le bot sur votre serveur`, emoji: "1163106292227915796", value: "invite-Bothelp" },
-                        { label: `Communauté`, description: `Rejoindre le serveur de support`, emoji: "1163106279187820544", value: "support-Bothelp" },
+                        {
+                            label: `Commandes`,
+                            description: `Afficher les commandes du bot !`,
+                            emoji: "1163106294073401354",
+                            value: "commands-Bothelp",
+                        },
+                        {
+                            label: `Invitation`,
+                            description: `Inviter le bot sur votre serveur`,
+                            emoji: "1163106292227915796",
+                            value: "invite-Bothelp",
+                        },
+                        {
+                            label: `Communauté`,
+                            description: `Rejoindre le serveur de support`,
+                            emoji: "1163106279187820544",
+                            value: "support-Bothelp",
+                        },
                     ]),
             );
 
         return client.embed({
-            title: `Aide du Bot`,
+            title: `Panneau d'aide`,
             image: 'https://i.imgur.com/cNHAdGT.png',
-            desc: `Bienvenue ! Choisissez une option dans le menu pour obtenir de l'aide.`,
+            desc: `Bienvenue dans le coin d'aide du bot ! J'ai concocté une p'tite présentation pour te guider. Choisis dans le menu, et on s'envole pour l'aventure !`,
             fields: [
-                { name: `Problèmes avec le menu ?`, value: `Signalez-le, et nous réglerons cela rapidement !` },
-                { name: `Bug repéré ?`, value: `[Signalez-le ici](https://discord.gg/7S28GvPsZQ) pour une résolution rapide !` },
+                {
+                    name: `Hé, le menu fait des siennes ? <:mugi_ooo:1163106314726158426>`,
+                    value: `<:mugi_happy:1163106301925142641> Pas de panique, on va régler ça ! Donne-nous un petit signe et on se charge de remettre tout sur pieds !`
+                },
+                {
+                    name: `Hé, vous avez dégoté un petit bug par ici ? <:mugi_ooo:1163106314726158426>`,
+                    value: `<a:mugi_pillow:1163075339405250571> Pas de souci, on va le chasser comme des pros ! [Signalez-le-nous](https://discord.gg/7S28GvPsZQ), et on s'occupe de le faire disparaître plus vite qu'un ninja dans l'ombre !`
+                },
             ],
-            components: [menu],
+            components: [row],
             type: 'editreply',
-        }, interaction);
+            ephemeral: true // Ajoutez cette ligne pour rendre la réponse éphémère
+        }, interaction)
     },
 };
