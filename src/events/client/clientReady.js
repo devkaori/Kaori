254478a9484792c81e6b03451596c799ed3d16e4
Masha <97138114+cosymasha@users.noreply.github.com>
2024-01-30 09:32:26 +0100

    Update clientReady.js

--- src/events/client/clientReady.js
+++ src/events/client/clientReady.js
@@ -11,7 +11,7 @@ module.exports = async (client) => {
     console.log(chalk.blue(chalk.bold(`Système`)), (chalk.white(`>>`)), chalk.red(`Fragment #${client.shard.ids[0] + 1}`), chalk.green(`est prêt !`))
     console.log(chalk.blue(chalk.bold(`Bot`)), (chalk.white(`>>`)), chalk.green(`Démarré sur`), chalk.red(`${client.guilds.cache.size}`), chalk.green(`serveurs !`))
 
-    let embed = new Discord.MessageEmbed()
+    let embed = new Discord.EmbedBuilder()
         .setTitle(`Fragment terminé`)
         .setDescription(`Un fragment vient de se terminer`)
         .addFields(
@@ -30,8 +30,8 @@ module.exports = async (client) => {
         const totalMembers = await client.shard.fetchClientValues('guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)');
         const totalEmotes = await client.shard.fetchClientValues('guilds.cache.reduce((acc, guild) => acc + guild.emojis.cache.size, 0)');
 
-        const statusText = `Sur ${totalGuilds.reduce((acc, guildCount) => acc + guildCount, 0)} serveurs | ${totalMembers.reduce((acc, memberCount) => acc + memberCount, 0)} membres | ${totalEmotes.reduce((acc, emoteCount) => acc + emoteCount, 0)} emotes`;
+        const statusText = `Sur ${totalGuilds.reduce((acc, guildCount) => acc + guildCount, 0)} serveurs`;
 
-        client.user.setPresence({ activities: [{ name: statusText, type: 'WATCHING' }], status: 'online' });
+        client.user.setPresence({ activities: [{ name: statusText, type: 'Participating' }], status: 'online' });
     }, 50000)
 }
