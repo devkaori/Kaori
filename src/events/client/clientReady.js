0ec274aaef5a433e1b650de62aa648a0b3bfe248
Masha <97138114+cosymasha@users.noreply.github.com>
2024-01-30 09:22:35 +0100

    Update and rename clientReady.ts to clientReady.js

---
+++ src/events/client/clientReady.js
@@ -0,0 +1,37 @@
+const Discord = require('discord.js');
+const chalk = require('chalk');
+
+module.exports = async (client) => {
+    const startLogs = new Discord.WebhookClient({
+        id: client.webhooks.startLogs.id,
+        token: client.webhooks.startLogs.token,
+    });
+
+    console.log(`\u001b[0m`);
+    console.log(chalk.blue(chalk.bold(`Système`)), (chalk.white(`>>`)), chalk.red(`Fragment #${client.shard.ids[0] + 1}`), chalk.green(`est prêt !`))
+    console.log(chalk.blue(chalk.bold(`Bot`)), (chalk.white(`>>`)), chalk.green(`Démarré sur`), chalk.red(`${client.guilds.cache.size}`), chalk.green(`serveurs !`))
+
+    let embed = new Discord.MessageEmbed()
+        .setTitle(`Fragment terminé`)
+        .setDescription(`Un fragment vient de se terminer`)
+        .addFields(
+            { name: "ID", value: `${client.shard.ids[0] + 1}/${client.options.shardCount}`, inline: true },
+            { name: "État", value: `Prêt`, inline: true },
+        )
+        .setColor(client.config.colors.normal);
+
+    startLogs.send({
+        username: 'Logs du Bot',
+        embeds: [embed],
+    });
+
+    setInterval(async function () {
+        const totalGuilds = await client.shard.fetchClientValues('guilds.cache.size');
+        const totalMembers = await client.shard.fetchClientValues('guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)');
+        const totalEmotes = await client.shard.fetchClientValues('guilds.cache.reduce((acc, guild) => acc + guild.emojis.cache.size, 0)');
+
+        const statusText = `Sur ${totalGuilds.reduce((acc, guildCount) => acc + guildCount, 0)} serveurs | ${totalMembers.reduce((acc, memberCount) => acc + memberCount, 0)} membres | ${totalEmotes.reduce((acc, emoteCount) => acc + emoteCount, 0)} emotes`;
+
+        client.user.setPresence({ activities: [{ name: statusText, type: 'WATCHING' }], status: 'online' });
+    }, 50000)
+}
