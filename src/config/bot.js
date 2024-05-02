3a3a980e1a81bc675ee10abc8ee08752f9167928
Kaori Café <dev@kaoricafe.fr>
2024-05-02 18:08:55 +0200

    Mis à jour

--- src/config/bot.js
+++ src/config/bot.js
@@ -9,6 +9,7 @@ module.exports = {
         id: process.env.DISCORD_ID,
         botInvite: `https://discord.com/oauth2/authorize?client_id=855107430693077033&permissions=633318697598967&scope=bot+applications.commands`,
         serverInvite: "https://discord.gg/HyybNmz2Ww",
+        prefix: "!"
     },
 
 }
